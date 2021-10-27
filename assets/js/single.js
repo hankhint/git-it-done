301aff571bbeb69e9d4a6003923c6fa3a36b3072
HankHint <hankhint@gmail.com>
2021-10-27 01:39:25 -0400

    added a warning message if over 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,11 +1,26 @@
+var limitWarningEl = document.querySelector("#limit-warning");
+
 var issueContainerEl = document.querySelector("#issues-container");
 
-var displayIssues = function(issues) {
-    if (issues.length === 0) {
-        issueContainerEl.textContent = "This repo has no open issues!";
-        return;
-    }
-for (var i=0; i < issues.length; i++) {
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+  };
+
+var displayIssues = function (issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+  for (var i = 0; i < issues.length; i++) {
     //create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
@@ -24,34 +39,36 @@ for (var i=0; i < issues.length; i++) {
 
     //check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
-        typeEl.textContent = "(Pull request)";
+      typeEl.textContent = "(Pull request)";
     } else {
-        typeEl.textContent = "(Issue)";
+      typeEl.textContent = "(Issue)";
     }
-    
+
     issueContainerEl.appendChild(issueEl);
-//append to container
-
+    //append to container
+  }
 };
-};
 
-var getRepoIssues = function(repo) {
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+var getRepoIssues = function (repo) {
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    fetch(apiUrl).then(function(response) {
-        //request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                //pass response data to dom function
-                displayIssues(data);
-            });
+  fetch(apiUrl).then(function (response) {
+    //request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        //pass response data to dom function
+        displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
         }
-        else {
-            alert("There was a problem with your request!");
-        }
-    });
+      });
+    } else {
+      alert("There was a problem with your request!");
+    }
+  });
 };
-
 
 // fetch(apiUrl).then(function(response) {
 //     //request was successful
